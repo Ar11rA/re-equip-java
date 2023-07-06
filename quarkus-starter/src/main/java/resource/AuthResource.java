8cb3ec121305699611f4c2bf8d78b37bd4da24e6
Aritra Ghosh <aritra119.26@gmail.com>
2023-07-06 17:50:41 +0530

    fix indent and params

--- quarkus-starter/src/main/java/resource/AuthResource.java
+++ quarkus-starter/src/main/java/resource/AuthResource.java
@@ -4,6 +4,7 @@ import dto.LoginRequest;
 import dto.TokenResponse;
 import services.AuthService;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
@@ -18,6 +19,7 @@ public class AuthResource {
 
     @POST
     @Path("/login")
+    @PermitAll
     public TokenResponse login(LoginRequest loginRequest) {
         return authService.login(loginRequest);
     }
--- quarkus-starter/src/main/java/resource/GreetingResource.java
+++ quarkus-starter/src/main/java/resource/GreetingResource.java
@@ -2,9 +2,13 @@ package resource;
 
 import services.GreetingService;
 
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
@@ -15,5 +19,13 @@ public class GreetingResource {
 
     public GreetingResource(GreetingService greetingService) {
         this.greetingService = greetingService;
+    }
+
+    @GET
+    @Path("/{name}")
+    @RolesAllowed("General")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greet(String name) {
+        return this.greetingService.greet(name);
     }
 }
